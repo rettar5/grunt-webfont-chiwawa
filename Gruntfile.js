bee85bb9fff3947bf8046316e12eaa523c6e67aa
rettar <rettar.v@gmail.com>
2017-12-09 17:49:26 +0900

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"), // package.jsonの読み込み
+    webfont: {
+      icons: {
+        src: "chiwawa_svgs/*.svg", // 変換元SVGファイルの指定
+        dest: "dist/fonts", // 生成したフォントの出力先
+        destCss: "dist/styles", // 生成したフォントに対応するスタイルシートの出力先
+        options: {
+          font: "chiwawa_fonts", // 生成したフォントのファイル名
+          engine: "node", // フォントの変換エンジン
+          stylesheet: "css", // スタイルシートの形式
+          htmlDemo: true, // 生成したフォントを確認できるデモページの有無
+          htmlDemoFilename: "chiwawa_fonts", // デモページのファイル名
+          types: "ttf,eot,woff", // 出力するフォントの形式
+          // template: "templates/style.scss",
+          templateOptions: {
+            baseClass: "chiwawa_fonts", // 生成するフォントの共通クラス
+            classPrefix: "chiwawa_fonts_" // 生成するフォントのクラス名のプレフィックス
+          },
+          // rename: function(name) {
+          //   // クラス名にスラッシュが入ってしまうため、ディレクトリ名を除外
+          //   return name.replace("Chiwawa_icons/", "");
+          // }
+        }
+      }
+    },
+  });
+  grunt.loadNpmTasks("grunt-webfont"); // プラグインの読み込み
+  grunt.registerTask("default", ["webfont"]); // デフォルトのタスクの設定
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# SVG to フォント変換サンプル
+
+[DA BLOG 「第一部 ベクタ形式のアイコンをWebフォントに変換する環境をつくる」](https://blog.dreamarts.co.jp)のサンプルプログラムです。
